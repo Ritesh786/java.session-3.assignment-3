c9c1155905af01f47b5cd9c96f655a8a201858b3
Ritesh786 <jharitesh6@gmail.com>
2016-07-20 22:15:59 +0530

    session 3.assignment3
    session 3.assignment3

---
+++ Ran.java
@@ -0,0 +1,11 @@
+
+public class Ran {
+
+	public static void main(String[]args){
+		Random r=new Random();
+		r.m=123;
+		r.num();
+	}
+}
+
+
---
+++ Random.java
@@ -0,0 +1,20 @@
+
+public class Random {
+	int i=0;
+	int m;
+	
+	void num(){
+		
+			if(i==0 && m>1){
+				int k=m-2;
+				System.out.println("A Random number =  "+k);
+			}
+			if(m==1||m==0){
+				
+				System.out.println(" A Random number is 0");
+			}
+		
+		}
+	}
+
+
